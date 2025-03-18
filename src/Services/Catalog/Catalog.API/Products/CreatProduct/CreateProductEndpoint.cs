294dfba379d0e3dcbe9898dcf4ab4d470ae691d6
Behnam <behnam.vosoogh@outlook.com>
2025-03-17 20:19:20 -0400

    Carter Library added and End poitt Request and response created

--- src/Services/Catalog/Catalog.API/Products/CreatProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreatProduct/CreateProductEndpoint.cs
@@ -1,5 +1,7 @@
 ﻿namespace Catalog.API.Products.CreatProduct
 {
+    public record createProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
     public class CreateProductEndpoint
     {
 
