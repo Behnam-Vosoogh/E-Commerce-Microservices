9e88d742c44fe907e405e0e1bc646d30978eb504
Behnam <behnam.vosoogh@outlook.com>
2025-03-20 16:40:35 -0400

    MediatR and Carter Libraries registerd into ASP.net

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(confige =>
+{
+    confige.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.MapCarter();
 
 app.Run();
