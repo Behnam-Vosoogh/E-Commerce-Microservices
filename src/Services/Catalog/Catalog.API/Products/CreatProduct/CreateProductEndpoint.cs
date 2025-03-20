909f46c7f53dbb77c7ab5504b3b8fdbb5cfc946e
Behnam <behnam.vosoogh@outlook.com>
2025-03-20 16:34:12 -0400

    post endpoint finished

--- src/Services/Catalog/Catalog.API/Products/CreatProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreatProduct/CreateProductEndpoint.cs
@@ -1,6 +1,4 @@
-﻿using Carter;
-
-namespace Catalog.API.Products.CreatProduct
+﻿namespace Catalog.API.Products.CreatProduct
 {
     public record createProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
     public record CreateProductResponse(Guid Id);
@@ -8,7 +6,18 @@ namespace Catalog.API.Products.CreatProduct
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            throw new NotImplementedException();
+            app.MapPost("products", async (createProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/product/{response.Id}",response);
+
+            }).WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
         }
     }
 }
