728121c0567184710ec0ead6321c217aacd7bc74
Behnam <behnam.vosoogh@outlook.com>
2025-03-25 00:05:02 -0400

    small change

--- src/Services/Catalog/Catalog.API/Products/CreatProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreatProduct/CreateProductHandler.cs
@@ -2,7 +2,7 @@
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandller(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
