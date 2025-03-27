31d363d0dbe27da5044ea971cb47a95bb71f5e11
Behnam <behnam.vosoogh@outlook.com>
2025-03-27 11:32:21 -0400

    GetProductById GET Endpoin added using minimal API and Carter library

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -2,5 +2,6 @@
 global using Mapster;
 global using MediatR;
 global using BuikdingBlocks.CQRS;
-global using Catalog.API.Models;
 global using Marten;
+global using Catalog.API.Models;
+global using Catalog.API.Exceptions;
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByQuery(int Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    internal class GetProductByIdQueryHandler
+        (IDocumentSession session,ILogger<GetProductByIdQueryHandler>logger)
+        : IQueryHandler<GetProductByQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}",query);
+            var product = await session.LoadAsync<Product>(query.Id.ToString(), cancellationToken);
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+}
