335171175c55313033571f3a28a4075ea096f42b
Behnam <behnam.vosoogh@outlook.com>
2025-03-30 16:12:33 -0400

    DeleteProductCommandHandler added

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductRequest(Guid Id):ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+
+
+    internal class DeleteProductCommandHandler(IDocumentSession session,ILogger<DeleteProductCommandHandler>logger):ICommandHandler<DeleteProductRequest, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductRequest command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+            return new DeleteProductResult(true);
+        }
+    }
+}
