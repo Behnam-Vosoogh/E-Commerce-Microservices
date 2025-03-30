59aeb386f478bd1cb317688e4c24ef569bff5636
Behnam <behnam.vosoogh@outlook.com>
2025-03-30 15:27:30 -0400

    small change

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -15,7 +15,7 @@ namespace Catalog.API.Products.GetProductById
                 var response = result.Adapt<GetProductByIdResponse>();
                 return Results.Ok(response);
             }).WithName("GetProductById")
-            .Produces<GetProductByIdResponse>(StatusCodes.Status201Created)
+            .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status400BadRequest)
             .WithSummary("Get Product By Id")
             .WithDescription("Get Product By Id");
